24f78dcfbc8d664bf4ae1e8646f1485af826b5e7
RafidMuhymin <rafidmuhymin@gmail.com>
2023-05-16 16:46:02 +0600

    added mdx and sass support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://astro.build`,
+  site: `https://astro.build`,
+
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    mdx(),
+  ],
 });
