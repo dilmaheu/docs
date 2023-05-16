987ec00e7961d94b0a39cbf4f7bd9063a894c46c
RafidMuhymin <rafidmuhymin@gmail.com>
2023-05-16 18:22:25 +0600

    removed OPEN_GRAPH export

--- src/consts.ts
+++ src/consts.ts
@@ -4,16 +4,6 @@ export const SITE = {
   defaultLanguage: "en-us",
 } as const;
 
-export const OPEN_GRAPH = {
-  image: {
-    src: "https://github.com/withastro/astro/blob/main/.github/assets/banner.png?raw=true",
-    alt:
-      "astro logo on a starry expanse of space," +
-      " with a purple saturn-like planet floating in the right foreground",
-  },
-  twitter: "astrodotbuild",
-};
-
 export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
