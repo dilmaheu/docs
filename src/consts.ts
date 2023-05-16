20fa7de48c44a265ba003f1e28de28ce2ead4270
RafidMuhymin <rafidmuhymin@gmail.com>
2023-05-16 18:25:17 +0600

    fixed github urls

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE = {
   defaultLanguage: "en-us",
 } as const;
 
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
